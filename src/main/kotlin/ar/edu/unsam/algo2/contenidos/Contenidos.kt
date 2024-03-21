2e49518d9690911dbd79627ba60656aae7bb1640
IlledNacu <bkilled7@gmail.com>
2024-03-21 02:28:08 -0300

    mejorados los metodos heredados

--- src/main/kotlin/ar/edu/unsam/algo2/contenidos/Contenidos.kt
+++ src/main/kotlin/ar/edu/unsam/algo2/contenidos/Contenidos.kt
@@ -2,17 +2,21 @@ package ar.edu.unsam.algo2.contenidos
 
 import java.time.LocalDate
 
-abstract class Contenido(val presupuesto: Int) {
-    open fun esInteresante() = presupuesto > 5
+abstract class Contenido(val presupuesto: Int){
+    //Template method
+    fun esInteresante() = presupuesto > 5 && cumpleCriterioInteresante()
 
+    //Primitive method
+    abstract fun cumpleCriterioInteresante(): Boolean
 }
 
 class Serie(presupuesto: Int, val temporadas: Int = 5) : Contenido(presupuesto) {
-    override fun esInteresante() = super.esInteresante() && temporadas < 5
+    //Primitive method
+    override fun cumpleCriterioInteresante() = temporadas < 5
 }
 
 class Pelicula(presupuesto: Int, val fechaEstreno: LocalDate = LocalDate.now()) : Contenido(presupuesto){
-    override fun esInteresante() = super.esInteresante() && seEstrenoRecientemente()
-
+    //Primitive method
+    override fun cumpleCriterioInteresante() = seEstrenoRecientemente()
     fun seEstrenoRecientemente() = fechaEstreno.year == LocalDate.now().year
 }
